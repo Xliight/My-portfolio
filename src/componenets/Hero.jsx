b99b65b84f099a0d6b4e071b1f6ee2f152b42db3
Xliight <abdomostme@gmail.com>
2024-07-16 08:51:52 +0100

    design updated

--- src/componenets/Hero.jsx
+++ src/componenets/Hero.jsx
@@ -15,8 +15,8 @@ const Hero = () => {
                 <div className="flex flex-col items-center lg:items-start" >
                     <motion.h1  variants={container(0)}
                     initial='hidden' animate='visible'
-                    className='pb-16 text-7xl font-thin tracking-tight lg:mt-16
-                       lg:text-5xl  ' >Abdelfettah MOSTAKIR</motion.h1>
+                    className='pb-16 text-7xl  tracking-tight lg:mt-16
+                       lg:text-5xl font-semibold ' >Abdelfettah MOSTAKIR</motion.h1>
                     <motion.span variants={container(0.3)}
                     initial='hidden' animate='visible'
                     className="bg-gradient-to-r from-pink-300 via-slate-500
